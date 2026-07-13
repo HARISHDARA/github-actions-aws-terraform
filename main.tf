2bca82f05f8eb187c33b904d83cff57c4dd208c5
HARISHDARA <31675455+HARISHDARA@users.noreply.github.com>
2026-07-13 11:56:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+module "oidc_useast1" {
+    source = "./modules/ec2"
+    ami_id = "ami-002192a70217ac181"
+    instance_type = "t2.nano"
+    key_name = "Tuesday"
+    instance_name = "east1"
+}
 
+
+output "east1_instance_id" {
+    value = module.oidc_useast1.instance_id
+}
+
+output "east1_public_ip" {
+    value = module.oidc_useast1.public_ip
+}
